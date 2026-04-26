3063b643b3df1f065e5ebd5f36b9d0a2104ba01c
Karthikh054 <hkarthik054@gmail.com>
2026-04-26 20:03:54 +0530

    changes in banner

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -5,24 +5,54 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import Image from "next/image";
-import { Navigation } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 function HomeSlider() {
   return (
     <div className="homeSlider">
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>
-          <div className="item">
-            <Image
-              src={"/slide1.png"}
-              alt="slide1"
-              width={1344}
-              height={514}
-              className="w-full"
-            />
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      <div className="container">
+        <Swiper
+          autoplay={{ delay: 2500, disableOnInteraction: false }}
+          centeredSlides={true}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="item">
+              <Image
+                src={"/slide1.png"}
+                alt="slide1"
+                width={1344}
+                height={514}
+                className="w-full"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="item">
+              <Image
+                src={"/slide1.png"}
+                alt="slide1"
+                width={1344}
+                height={514}
+                className="w-full"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="item">
+              <Image
+                src={"/slide1.png"}
+                alt="slide1"
+                width={1344}
+                height={514}
+                className="w-full"
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
